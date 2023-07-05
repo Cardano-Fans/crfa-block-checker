3966a25e8ed53ec3f69d3494d715bad592f35f73
Robin Böning <robin.boening@gmail.com>
2023-07-05 22:42:50 +0200

    support cardano-cli leadership-schedule format

--- crfa-block-checker.rb
+++ crfa-block-checker.rb
@@ -1,6 +1,15 @@
+require 'time'
 require 'bundler/setup'
 Bundler.require(:default) # require all bundled gems
 Dotenv.load
+
+SHELLY_SLOT = 4492800
+SHELLEY_EPOCH = 208
+SLOTS_PER_EPOCH = 432000
+
+def epoch_for_slot(slot)
+  SHELLEY_EPOCH + ((slot - SHELLY_SLOT) / SLOTS_PER_EPOCH)
+end
 
 blockfrost_project_id = ENV.fetch('BLOCKFROST_MAINNET_KEY')
 blockfrost = Blockfrostruby::CardanoMainNet.new(blockfrost_project_id)
@@ -11,14 +20,27 @@ puts 'Latest slot: ' + latest_slot.to_s
 input_file = ARGV[0] || Dir["./epochs/*"].sort.last
 pool_id = ARGV[1] || ENV.fetch('POOL_ID')
 
-file = File.read(input_file)
+assignedSlots = {}
 
-leaderSlotFileJson = JSON.parse(file)
+begin
+    # `cncli leaderlog` format
+    cncliData = JSON.parse(File.read input_file)
+    cncliData["assignedSlots"].each do |slot|
+        assignedSlots[slot["slot"]] = Time.parse(slot["at"]).utc
+    end
+rescue JSON::ParserError
+    # `cardano-cli query leadership-schedule` format
+    File.open(input_file).each do |line|
+      m = line.match /^\s*(?<slot>\d*)\s*(?<time>.*)/
+      if m && !m[:slot].empty?
+        assignedSlots[m[:slot].to_i] = Time.parse(m[:time]).utc
+      end
+    end
+end
 
-epochNo = leaderSlotFileJson['epoch']
-epochSlots = leaderSlotFileJson['epochSlots']
-
-assignedSlots = leaderSlotFileJson['assignedSlots']
+epochNo = epoch_for_slot(assignedSlots.keys.first)
+currentEpochNo = epoch_for_slot(latest_slot)
+epochSlots = assignedSlots.size
 
 if epochSlots == 0
     puts 'No slots allocated for epochNo:' + epochNo
@@ -31,15 +53,13 @@ slotBattleLost = 0
 stillToMint = 0
 
 # this variable represents a state which indicates if we are currently running block checker for epoch that is in progress
-currentlyRunningEpoch = false
+currentlyRunningEpoch = epochNo == currentEpochNo
 
 puts "Slots allocated: #{epochSlots} for epochNo: #{epochNo}"
 
 puts "Checking if slots filled by blocks..."
 
-assignedSlots.each do |item|
-    slot = item["slot"]
-    at = item["at"]
+assignedSlots.each do |slot, slotTime|
     block = blockfrost.get_block_in_slot(slot)
     status = block[:status]
     if slot < latest_slot # it makes sense to check only past slots
@@ -54,15 +74,13 @@ assignedSlots.each do |item|
             end
         elsif status == 404
             heightBattleLost += 1
-            puts "Block ghosted on slot: #{slot} due to a height battle at #{at}."
+            puts "Block ghosted on slot: #{slot} due to a height battle at #{slotTime}."
         elsif
             puts "Unknown status: #{} for slotNo: #{slot}"
         end
     else
-        # if we are here it means there were some blocks which are to be minted in the future
-        currentlyRunningEpoch = true
         stillToMint += 1
-        #puts "Block on slot #{slot} at #{at} will be minted in the future."
+        puts "Block on slot #{slot} at #{slotTime} will be minted in the future."
     end
 end
 
